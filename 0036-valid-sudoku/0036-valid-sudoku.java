8cd852e7629709382534e93b3cf553f470b49fb1
Naveen-Arul <154906534+Naveen-Arul@users.noreply.github.com>
2026-02-02 21:12:15 +0530

    Time: 2 ms (67.19%), Space: 46.6 MB (49.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,7 +3,6 @@ class Solution {
         HashSet<Character>[] rows = new HashSet[9];
         HashSet<Character>[] cols = new HashSet[9];
         HashSet<Character>[] boxes = new HashSet[9];
-        // initialize all sets
         for (int i = 0; i < 9; i++) {
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
@@ -14,13 +13,10 @@ class Solution {
                 char ch = board[i][j];
                 if (ch == '.') continue;
                 int boxIndex = (i / 3) * 3 + (j / 3);
-                // check row
                 if (rows[i].contains(ch)) return false;
                 rows[i].add(ch);
-                // check column
                 if (cols[j].contains(ch)) return false;
                 cols[j].add(ch);
-               // check 3x3 box
                 if (boxes[boxIndex].contains(ch)) return false;
                 boxes[boxIndex].add(ch);
             }
