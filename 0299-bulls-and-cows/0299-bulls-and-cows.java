277d4afc2feddfddd518664981d95c26d70c30e0
NAVEEN A <154906534+Naveen-Arul@users.noreply.github.com>
2026-06-17 14:06:28 +0530

    Time: 3 ms (93.36%), Space: 43.5 MB (70.59%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,28 +1,23 @@
 class Solution {
     public String getHint(String secret, String guess) {
-
-        int bulls = 0;
-        int cows = 0;
-
-        int[] sCount = new int[10];
-        int[] gCount = new int[10];
-
-        for (int i = 0; i < secret.length(); i++) {
-            char s = secret.charAt(i);
-            char g = guess.charAt(i);
-
-            if (s == g) {
-                bulls++; // correct digit + correct position
-            } else {
-                sCount[s - '0']++;
-                gCount[g - '0']++;
+        int bulls=0;
+        int cows=0;
+        int[] scount=new int[10];
+        int[] gcount=new int[10];
+        for(int i=0;i<secret.length();i++){
+            char s=secret.charAt(i);
+            char g=guess.charAt(i);
+            if(s==g){
+                bulls++;
+            }
+            else{
+                scount[s-'0']++;
+                gcount[g-'0']++;
             }
         }
-
-        for (int i = 0; i < 10; i++) {
-            cows += Math.min(sCount[i], gCount[i]);
+        for(int i=0;i<10;i++){
+            cows+=Math.min(scount[i],gcount[i]);
         }
-
-        return bulls + "A" + cows + "B";
+        return bulls+"A"+cows+"B";
     }
 }
